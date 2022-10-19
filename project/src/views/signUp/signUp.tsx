276a90611b91ad9ca18d85fee49349a669e7d108
Kavita <kavita.chaudhary@robosoftin.com>
2022-10-19 16:55:57 +0530

    saved

--- project/src/views/signUp/signUp.tsx
+++ project/src/views/signUp/signUp.tsx
@@ -7,12 +7,12 @@ function SignUp() {
     <div className='rectangle'>
     <Logo/>
     <div className='formContainer'>
-        <form action="" className='form'>
+        <form action="/" className='form'>
             <div className='sign-up'>SIGN UP</div>
-            <input type="tel" name="" id="" placeholder='Enter Mobile Number' />
-            <input type="text" name="" id="" placeholder='Enter 4 Digit MPin' />
-            <input type="text" name="" id="" placeholder='Re-Enter 4 Digit MPin' />
-            <button className='button-background'> <span className='signup'> <Link to="/congratulation">SIGN UP</Link> </span></button>
+            <input type="tel" name="" id="" required minLength={10} placeholder='Enter Mobile Number'/>
+            <input type="text" name="" id="" required minLength={4} placeholder='Enter 4 Digit MPin'/>
+            <input type="text" name="" id="" required minLength={4} placeholder='Re-Enter 4 Digit MPin'/>
+            <button className='button-background' type='submit'> <span className='signup'>SIGN UP </span></button>
         </form>
     </div>
    </div>
